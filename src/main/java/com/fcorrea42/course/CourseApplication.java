181f11220f8e51548c732f1bbff76ad396750d3a
Felipe de Freitas Correa <fcorreafelipe@gmail.com>
2019-10-10 10:35:43 -0300

    Project created

---
+++ src/main/java/com/fcorrea42/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.fcorrea42.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
